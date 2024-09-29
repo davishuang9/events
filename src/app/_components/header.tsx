c7fa4c130a884178e2a1941ecccd21e5b5441a35
Davis Huang <davis@mosey.com>
2024-09-29 16:12:34 -0700

    Add homepage, account navigator

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { AccountDropdown } from "@/components/account-dropdown";
+import { Session } from "node_modules/next-auth/core/types";
+
+type HeaderProps = {
+  session: Session | null;
+};
+
+export function Header({ session }: HeaderProps) {
+  return (
+    <div className="absolute top-0 flex w-screen justify-between">
+      <div className="p-4">
+        <span className="text-xl">1️⃣</span>
+        <span className="ml-2 text-xl font-bold text-indigo-600">onestop</span>
+      </div>
+      <AccountDropdown session={session} />
+    </div>
+  );
+}
---
+++ src/components/account-dropdown.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, Calendar } from "lucide-react";
+import Link from "next/dist/client/link";
+import { Session } from "node_modules/next-auth/core/types";
+
+type AccountDropdownProps = {
+  session: Session | null;
+};
+
+export function AccountDropdown({ session }: AccountDropdownProps) {
+  return (
+    <div className="flex justify-end p-4">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="text-2xl" variant="outline" size="icon">
+            ☰
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end">
+          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+          {/* <DropdownMenuSeparator /> */}
+          {session && (
+            <>
+              <DropdownMenuItem>
+                <Calendar className="mr-2 h-4 w-4" />
+                <span>Events</span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+            </>
+          )}
+          {/* <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <HelpCircle className="mr-2 h-4 w-4" />
+            <span>Help</span>
+          </DropdownMenuItem> */}
+          <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+            <DropdownMenuItem>
+              <LogOut className="mr-2 h-4 w-4" />
+              {session ? "Sign out" : "Sign in"}
+            </DropdownMenuItem>
+          </Link>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
